7427203be02c730c2b6c4b8e2bd0de5a1916c8b9
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-08-20 18:18:00 -0500

    added different steps on the homepage for the dragging animation.

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -10,7 +10,7 @@ const HomeInfo = ({ currentStage }) => {
                 <span className='font-semibold mx-2 text-white'>Pratik</span>
                 👋
                 <br />
-                A Software Engineer
+                A Software Engineer & Designer
             </h1>
         );
 
@@ -18,12 +18,12 @@ const HomeInfo = ({ currentStage }) => {
         return (
             <div className='info-box'>
                 <p className='font-medium sm:text-xl text-center'>
-                    Worked with many companies 
+                    Worked with many companies <br /> and picked up many skills along the way
                 </p>
 
                 <Link to='/about' className='neo-brutalism-white neo-btn'>
                     Learn more
-                    <img alt='arrow' className='w-4 h-4 object-contain' />
+                    <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
                 </Link>
             </div>
         );
