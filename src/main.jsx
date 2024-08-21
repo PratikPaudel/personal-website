d70be3918777af862d9e2cb6dc8c3d6ae8a15c98
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-08-21 11:13:11 -0500

    added speed insight from vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 ﻿import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import {injectSpeedInsights} from "@vercel/speed-insights";
 
+injectSpeedInsights();
 ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
